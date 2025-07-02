89376469d742df91096941239d6fac2478ee2bad
Fiorella Abarca <tuemail@example.com>
2025-07-02 15:04:06 -0600

    feat: modificacion de clases procesador_detos papa

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-ECHO est  activado.
+from procesador_rnn import ProcesadorDatosPapa
+
+if __name__ == "__main__":
+    # Crear instancia del procesador
+    archivo = r"C:\Users\Fiorella Victoria\PycharmProjects\PythonProject12\ESTIM_papa_2005-2025 (1).xls"
+    procesador = ProcesadorDatosPapa(archivo)
+
+    # Procesar directamente a formato largo y exportar
+    procesador.procesar_y_exportar("datos_transformados.csv")
